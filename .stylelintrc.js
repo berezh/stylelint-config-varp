0d621939a16d23d775aa8a0a0b6c6516a3939d11
unknown <verezh@gmail.com>
2023-01-17 00:07:51 +0200

    update

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -30,6 +30,14 @@ module.exports = {
         "indentation": 2,
         'block-opening-brace-space-before': ["always"],
         "color-function-notation": ["legacy"],
+        // https://stylelint.io/user-guide/regex/#enforce-a-case
+        /* Additional rules */
+        "custom-property-pattern": [
+            "^[a-z][a-zA-Z0-9]+$",
+            {
+              message: "Expected custom property name to be camel-case",
+            },
+        ],
         // ORDER
         "order/order": [
             "custom-properties",
